1fa73a75c57195e002b0bdb6ece3125d24a9b131
Manuel <64737793+ardizio@users.noreply.github.com>
2024-02-01 22:15:33 +0100

    Create BlogPostContainerView.php

---
+++ demo/working_blocks/ReusableWidgets/BlogPostContainerView.php
@@ -0,0 +1,11 @@
+<a class="VStack" data-uuid="" data-SwiftUI=".onTapGesture(perform:Route_ArticleReaderView/@uuid).stack(alignment:leading).fill(Tinted_TextCOLOR_heading).cornerRadius(window.screen.width<720?0:4)">
+    <div class="Text" data-value="articleTitle" data-SwiftUI=".fontExtension(weight:3;size:4).foregroundColor(2).customPosition(pad:16,12,4).lineSpacing(1.15)"></div>
+    <div class="Text" data-value="articleDescription" data-SwiftUI=".fontExtension(weight:2;size:6).foregroundColor(3).customPosition(pad:0,12)"></div> 
+    <div class="HStack" data-SwiftUI=".stack(spacing:4).customPosition(pad:4,12,16)">
+        <div class="Text" data-value="articleDate" data-SwiftUI=".fontExtension(weight:2;size:7).foregroundColor(3)"></div>  
+        <div class="Text" data-SwiftUI=".fontExtension(weight:2;size:7).foregroundColor(3)">•</div>  
+        <div class="Text" data-value="articleReadingTime" data-SwiftUI=".fontExtension(weight:2;size:7).foregroundColor(3)"></div>  
+        <div class="Text" data-SwiftUI=".fontExtension(weight:2;size:7).foregroundColor(3)">•</div>  
+        <div class="Text" data-value="articleAuthor" data-SwiftUI=".fontExtension(weight:2;size:7).foregroundColor(3)"></div>  
+    </div> 
+</a>
